36d33b7806b049b2c0405d68d4cf72b185ac8585
Ryan <rwealleans@aoiths.org>
2018-01-22 23:15:49 +0000

    project

--- src/Message.js
+++ src/Message.js
@@ -1,20 +1,25 @@
 import './Message.css';
 import React from 'react';
+import base64 from 'base-64';
 
 export default class Message extends React.Component {
-  render() {
-    var renderedDate = (new Date(this.props.value.timestamp)).toLocaleString();
-
-    return (
-      <div className="Message">
-        <div>
-          <span className="Message-author">{this.props.value.author}</span>
-          <span className="Message-timestamp">
-            {renderedDate}
-          </span>
-        </div>
-        <div className="Message-contents">{this.props.value.contents}</div>
-      </div>
-    );
-  }
-}
+    render() {
+      var renderedDate = (new Date(this.props.value.timestamp)).toLocaleString();
+      var textContents = this.props.value.is_encrypted? base64.decode(this.props.value.contents) : this.props.value.contents;
+      var textContentsClass = this.props.value.is_encrypted ? "Message-contents-secret" : "Message-contents";
+      var renderedContents = this.props.value.image_source ? 
+           <div className={textContentsClass}><img src={this.props.value.image_source} alt={textContents} /></div> :
+           <div className={textContentsClass}>{textContents}</div>;
+      return (
+        <div className="Message">
+          <div>
+           <span className="Message-author">{this.props.value.author}</span>
+           <span className="Message-timestamp">
+             {renderedDate}
+           </span>
+         </div>
+         {renderedContents}
+       </div>
+     );
+   }
+ }
--- src/MessageInput.js
+++ src/MessageInput.js
@@ -1,57 +1,75 @@
 import './MessageInput.css';
-import 'firebase/auth';
-import Button from 'material-ui/Button';
-import Input from 'material-ui/Input';
-import React from 'react';
-import base from './rebase';
-import firebase from 'firebase/app';
-
-export default class MessageInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {message: ''};
-    this.user = {};
-  }
-  componentDidMount() {
-    var self = this;
-    firebase.auth().onAuthStateChanged(function(user) {
-      self.user = user;
-    });
-  }
-  sendMessage(event) {
-    // var self = this; // This will be needed later.
-    event.preventDefault();
-    if(!this.state.message) {
-      return;
+ import 'firebase/auth';
+ import Button from 'material-ui/Button';
+ import Input from 'material-ui/Input';
+  import React from 'react';
+  import base from './rebase';
+  import firebase from 'firebase/app';
+  import base64 from 'base-64';
+  
+  export default class MessageInput extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {message: '', secretMode: false};
+      this.user = {};
     }
-    base.push('channels/' + this.props.channelId + '/messages', {
-      data: {
-        author: this.user.displayName,
-        contents: 'TODO replace this string with the message input text',
-        timestamp: Date.now(),
-      },
-      then: function(err) {
-        if(err) {
-          console.error('Error saving message', err);
-        } else {
-          // TODO reset the message input to empty.
-        }
-      }
-    });
+    componentDidMount() {
+     var self = this;
+     firebase.auth().onAuthStateChanged(function(user) {
+       self.user = user;
+     });
+   }
+   sendMessage(event) {
+     var self = this;
+     event.preventDefault();
+     if(!this.state.message) {
+       return;
+     }
+      base.push('channels/' + this.props.channelId + '/messages', {
+        data: {
+          author: this.user.displayName,
+          contents: this.state.secretMode ? base64.encode(this.state.message) 
+                                          : this.state.message,
+          timestamp: Date.now(),
+          is_encrypted: this.state.secretMode
+        },
+        then: function(err) {
+          if(err) {
+           console.error('Error saving message', err);
+         } else {
+           self.setState({'message': ''});
+         }
+       }
+     });
+   }
+    handleChange(event) {
+      this.setState({'message': event.target.value});
+    }
+    toggleSecretMode(event) {
+      this.setState({'secretMode': !this.state.secretMode});
+    }
+    
+    render() {
+      var secretModeText = this.state.secretMode ? "BACK TO NORMAL" : "ENABLE SECRET MODE";
+      var sendButtonText = this.state.secretMode ? "SEND SECRET" : "SEND";
+      var secretText = this.state.secretMode ? <div className="MessageInput">Secret text: {base64.encode(this.state.message)}</div> : <div />;
+      return (
+        <div>
+          {secretText}
+          <form className="MessageInput" onSubmit={this.sendMessage.bind(this)}>
+            <Input className="MessageInput-input" placeholder="Send Message"
+              value={this.state.message} onChange={this.handleChange.bind(this)}
+              autoFocus={true}/>
+            <Button type="submit" raised color="primary">
+              {sendButtonText}
+            </Button>
+            <Button type="button" raised color="secondary"
+              onClick={this.toggleSecretMode.bind(this)}>
+              {secretModeText}
+            </Button>
+          </form>
+         
+        </div>
+      );
+    }
   }
-  handleChange(event) {
-    // TODO set the state message property to event.target.value
-  }
-  render() {
-    return (
-      <form className="MessageInput" onSubmit={this.sendMessage.bind(this)}>
-        <Input className="MessageInput-input" placeholder="Send Message"
-          value={this.state.message} onChange={this.handleChange.bind(this)}
-          autoFocus={true}/>
-        <Button type="submit" raised color="primary">
-          SEND
-        </Button>
-      </form>
-    );
-  }
-}
